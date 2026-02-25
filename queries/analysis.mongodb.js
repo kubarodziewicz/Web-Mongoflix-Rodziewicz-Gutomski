c3ad334cc48204d8215d5115572d4bc720599bef
kubarodziewicz <kubarodziewicz@cyfroweszkoly.pl>
2026-02-25 09:12:31 +0100

    create script #2 - list of average ratings for movies

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -1,1 +1,13 @@
+db = db.getSiblingDB("Movies")
 
+print("--- Analysis --- ")
+
+print("#2 Lista średnich ocen filmów")
+const average_rating = db.movie.aggregate([
+    {$lookup: {from: "reviews", localField: "title", foreignField: "movie_title", as: "review"}},
+    {$unwind: {path: "$review"}},
+    {$group: {_id: "$title", average_rating: {$avg: "$review.rating"}}},
+    {$set: {average_rating: {$round: ["$average_rating", 1]}}}
+]).toArray()
+
+printjson(average_rating)
